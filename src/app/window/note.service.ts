a9820399355576c37979987aa4b4572e45a1782b
Aylie Chou <aylie_chou@openfind.com.tw>
2019-06-24 10:38:40 +0800

    [Summary] refactor note.service [Type] refactor [Scope] note.service [Bug] [Phenomemon]
    [Reason]
    
    [Changes]
    
    [Announcement]

--- src/app/window/note.service.ts
+++ src/app/window/note.service.ts
@@ -11,30 +11,6 @@ import * as _ from 'lodash';
 export class NoteService {
 
   notes: Note[];
-/*
-  notes: Note[] = [
-    {
-      id: 1,
-      title: 'title1title1title1title1title1title1title1title1title1title1',
-      content: 'content1',
-      color: 'red',
-      coordinate: {
-        x: 100,
-        y: 100,
-      }
-    },
-    {
-      id: 2,
-      title: 'title2',
-      content: 'content2',
-      color: 'blue',
-      coordinate: {
-        x: 400,
-        y: 400,
-      }
-    }
-  ];
-*/
 
   constructor() { }
 
@@ -43,11 +19,7 @@ export class NoteService {
       return of(this.notes);
     }
     let cacheStr: string = localStorage.getItem('notes');
-    if (!cacheStr) {
-      this.notes = [];
-      return of(this.notes);
-    }
-    let cache: Note[] = Array.from(JSON.parse(cacheStr)) || [];
+    let cache: Note[] = cacheStr ? (Array.from(JSON.parse(cacheStr)) || []) : [];
     this.notes = cache;
     return of(cache);
   }
